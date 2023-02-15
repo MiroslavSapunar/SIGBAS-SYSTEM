79783a10500e78302a08974bc977b65a00e1c31b
MiroslavSapunar <psapunar@fi.uba.ar>
2023-02-15 20:29:13 -0300

    SIGBAS official pantone

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0095db',
+        secondary: '#80549e'
+      }
+    },
   },
   plugins: [],
 }
