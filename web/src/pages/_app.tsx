211b975102db1b0dc2af259fd005f7105476e6f7
MiroslavSapunar <psapunar@fi.uba.ar>
2023-02-17 16:47:00 -0300

    Refactor Layout

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import Layout from '@/components/layout'
+// import { Component } from 'react'
+
+// export default function App({ Component, pageProps }: AppProps) => {
+//   return
+
+// }
+
+const SIGBAS_SYSTEM = ({ Component }: AppProps) =>
+  <Layout>
+    <Component />
+  </Layout>
+
+export default SIGBAS_SYSTEM
