4881bbfd6dca62ce56ce131f1b6102285191c565
MiroslavSapunar <psapunar@fi.uba.ar>
2023-03-31 01:13:32 -0300

    middleware refactor

---
+++ web/src/middlewares/StudentAccess.ts
@@ -0,0 +1,32 @@
+import { getToken } from "next-auth/jwt";
+import { NextFetchEvent, NextMiddleware, NextRequest, NextResponse } from "next/server";
+import { MiddlewareFactory } from "./types";
+
+export const studentAccess: MiddlewareFactory = (next: NextMiddleware) => {
+    return async (request: NextRequest, _next: NextFetchEvent) => {
+        const res = await next(request, _next);
+        if (res) {
+            const { pathname } = request.nextUrl
+
+            const accessPaths = ["/dashboard/estudiante"]
+
+            const matchesProtectedPath = accessPaths.some((path) => pathname.startsWith(path))
+
+            if (matchesProtectedPath) {
+                const token = await getToken({ req: request })
+                if (!token) {
+                    const url = new URL(`/signin`, request.url)
+                    url.searchParams.set("callbackUrl", encodeURI(request.url))
+                    return NextResponse.redirect(url)
+                }
+                if (token.role !== "Student") {
+                    const url = new URL(`/403`, request.url)
+                    return NextResponse.rewrite(url)
+                }
+            }
+            // return NextResponse.next()
+        }
+        return res;
+    };
+};
+
